6710029aa30f7362ad95d9c45947c567eabe2e2f
Nate Matykiewicz <natem@auctionedge.com>
2016-04-16 13:51:33 -0500

    Make page an off-white color

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # dyslexic-bible
 
 This is a simple [Greasemonkey](http://www.greasespot.net/) script to change the font of
-[bible.com](https://www.bible.com/bible) to the [Dyslexie](https://www.dyslexiefont.com) font.
+[bible.com](https://www.bible.com/bible) to the [Dyslexie](https://www.dyslexiefont.com) font
+and makes the page an off-white color.
 You must have the font installed on your computer already.
 
 To use with Chrome try
--- dyslexic-bible.user.js
+++ dyslexic-bible.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         dyslexic-bible
 // @namespace    http://tampermonkey.net/
-// @version      1
+// @version      2
 // @description  Use a dyslexic-friendly font at bible.com
 // @match        https://*.bible.com/bible/*
 // updateURL     https://github.com/natematykiewicz/dyslexic-bible/raw/master/dyslexic-bible.user.js
@@ -9,6 +9,9 @@
 
 (function() {
     $(document).ready(function(){
-      $('#current-ui-view').css('font-family', 'Dyslexie');
+      $('#current-ui-view')
+        .css('font-family', 'Dyslexie')
+        .css('background', '#fbf7e9')
+        .css('color', '#4d4433');
     });
 })();
